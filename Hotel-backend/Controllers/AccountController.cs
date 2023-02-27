5e0e5e1f92fbe7596aac20839acfb6387d064ff9
Tarsem Singh <tarsem.it@gmail.com>
2023-02-28 01:18:18 +0530

    Moved project to new folder

---
+++ Hotel-backend/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Api.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Service;
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+
+    public AccountController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateAdmin()
+    {
+        await _accountService.CreateAdminAccount();
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> ChangePassword(ChangePasswordRequest model)
+    {
+        await _accountService.ChangePassword("", model.CurrentPassword, model.NewPassword);
+        return Ok();
+
+    }
+
+
+
+}
