3398bcd755cc049d0f2b42d9d74029ccca3da573
Tarsem Singh <tarsem.it@gmail.com>
2023-03-29 01:25:50 +0530

    class is working fine now

--- Hotel-Web/src/app/shared/class/class.module.ts
+++ Hotel-Web/src/app/shared/class/class.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AddClassComponent } from './add-class/add-class.component';
 import { ClassService } from './class.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatDatepickerModule} from "@angular/material/datepicker"
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [AddClassComponent],
-  imports: [CommonModule,ReactiveFormsModule,MatDatepickerModule],
+  imports: [CommonModule, ReactiveFormsModule, MatDatepickerModule],
   providers: [ClassService],
 })
 export class ClassModule {}
