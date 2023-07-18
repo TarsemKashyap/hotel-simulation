64da057812ffe6bbde61208a4d2af3d4c707d650
puneetpanwar26 <34851078+puneetpanwar26@users.noreply.github.com>
2023-07-18 18:36:56 +0530

    pricedecision,RoomAllocation etc some Table Columns Update

--- Hotel-backend/Database/Migrations/20230718130351_priceRoomSoldColumnsUpdate.Designer.cs
+++ Hotel-backend/Database/Migrations/20230718130351_priceRoomSoldColumnsUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Database.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230718130351_priceRoomSoldColumnsUpdate")]
+    partial class priceRoomSoldColumnsUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Hotel-backend/Database/Migrations/20230718130351_priceRoomSoldColumnsUpdate.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    public partial class priceRoomSoldColumnsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CustomerRating",
+                table: "WeightedAttributeRating",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceExpectation",
+                table: "WeekdayVSsegmentConfig",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Revenue",
+                table: "SoldRoomByChannel",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cost",
+                table: "SoldRoomByChannel",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Revenue",
+                table: "RoomAllocation",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupID",
+                table: "PriceDecision",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerRating",
+                table: "WeightedAttributeRating",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PriceExpectation",
+                table: "WeekdayVSsegmentConfig",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Revenue",
+                table: "SoldRoomByChannel",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Cost",
+                table: "SoldRoomByChannel",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Revenue",
+                table: "RoomAllocation",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupID",
+                table: "PriceDecision",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
