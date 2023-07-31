a51f7d04fa4408986322b90d963e301ae3dbdfa8
puneetpanwar26 <34851078+puneetpanwar26@users.noreply.github.com>
2023-07-31 21:59:36 +0530

    Table Customer Row Ratting Update

---
+++ Hotel-backend/Database/Migrations/20230731084301_customerRowRattingUpdate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    public partial class customerRowRattingUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RawRating",
+                table: "CustomerRawRating",
+                type: "decimal(4,2)",
+                precision: 4,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RawRating",
+                table: "CustomerRawRating",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(4,2)",
+                oldPrecision: 4,
+                oldScale: 2);
+        }
+    }
+}
