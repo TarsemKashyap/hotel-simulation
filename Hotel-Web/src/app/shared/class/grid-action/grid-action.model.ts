e7446c2e51f9e3fc86c21321a3ef6fa853b54c18
Tarsem Singh <tarsem.it@gmail.com>
2023-10-02 19:22:35 +0530

    report link

--- Hotel-Web/src/app/shared/class/grid-action/grid-action.model.ts
+++ Hotel-Web/src/app/shared/class/grid-action/grid-action.model.ts
@@ -7,6 +7,7 @@ export interface RowAction {
   cssClass?: string;
   hide: ($event: Event, row: any) => boolean;
   onClick: ($event: Event, row: any) => void;
+  tooltip?:string;
 }
 
 declare type ActionBtnType = 'text' | 'icon';
